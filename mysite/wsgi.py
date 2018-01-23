f101b242abd3a5560700a85f4b32a2edd98063bb
ruhidamda <ruhidamda@gmail.com>
2018-01-22 19:35:09 -0600

    Python anywhere setting

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -8,14 +8,10 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
-import sys
-
-path = os.path.expanduser('~/Aysha8210Assign1pa')
-if path not in sys.path:
-    sys.path.append(path)
-
-os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
 
 from django.core.wsgi import get_wsgi_application
-from django.contrib.staticfiles.handlers import StaticFilesHandler
-application = StaticFilesHandler(get_wsgi_application())
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+
+application = get_wsgi_application()
+
